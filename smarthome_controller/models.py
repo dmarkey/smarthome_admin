efb69637be0de85afe7b809bcd5e62e27e349827
David Markey <david@dmarkey.com>
2015-05-11 14:58:22 +0100

    Initial.

--- smarthome_controller/models.py
+++ smarthome_controller/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+TYPES = (
+    (1, "ESP8266"),
+)
+
+STATUSES = (
+    (0, "CREATED"),
+    (1, "SENT"),
+    (2, "ACKNOWLEDGED"),
+    (2, "COMPLETE"),
+
+)
+
+
+class ControllerCapability(models.Model):
+    name = models.CharField(max_length=1024)
+    description = models.TextField()
+
+
+class SmartHomeController(models.Model):
+    unique_id = models.CharField(max_length=1024)
+    first_registered = models.DateTimeField(auto_created=True)
+    type = models.IntegerField(choices=TYPES)
+    capabilities = models.ManyToManyField(ControllerCapability)
+
+
+class ControllerPing(models.Model):
+    controller = models.ForeignKey(SmartHomeController)
+    time = models.DateTimeField(auto_created=True)
+
+
+class ControllerTask(models.Model):
+    controller = models.ForeignKey(SmartHomeController)
+    description = models.CharField(max_length=4096)
+    capability = models.ForeignKey(ControllerCapability)
+    arguments = models.CharField(max_length=1024)
+    status = models.SmallIntegerField(default=0, choices=STATUSES)
+
+
+
+
+
+
+
+
+
+
