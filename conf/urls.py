e356884eb7f84a82adc57fe11de5b2138c211725
David Markey <david@dmarkey.com>
2015-05-11 17:27:23 +0100

    Interation

---
+++ conf/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from api import views
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'untitled.views.home', name='home'),
+    # url(r'^untitled/', include('untitled.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^controller_ping_create/$', views.ControllerPingCreate.as_view()),
+)
